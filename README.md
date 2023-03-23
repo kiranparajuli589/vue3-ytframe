5ee07d7159343e1cf5a8b7daa76f4da2a02e17b3
Parajuli Kiran <kiranparajuli589@gmail.com>
2023-03-23 15:34:59 +0545

    Added about page in the docs

--- README.md
+++ README.md
@@ -7,11 +7,11 @@
 <a href="https://www.npmjs.com/package/vue3-ytframe"><img src="https://img.shields.io/npm/v/vue3-ytframe.svg"/> <img src="https://img.shields.io/npm/dm/vue3-ytframe.svg"/></a> <a href="https://vuejs.org/"><img src="https://img.shields.io/badge/vue-3-brightgreen.svg"/></a>
 </p>
 
+## Documentation
+https://kiranparajuli589.github.io/vue3-ytframe/#/docs/ref=
+
 ## Playground
 https://kiranparajuli589.github.io/vue3-ytframe/#/playground
 
 ## Usage Examples
 https://kiranparajuli589.github.io/vue3-ytframe/#/docs/ref=examples
-
-## Documentation
-https://kiranparajuli589.github.io/vue3-ytframe/#/docs/ref=
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,3 +1,36 @@
 <template>
-	<div>About</div>
+	<div class="about">
+		<h2>About VueYtframe</h2>
+		<hr>
+		<div>Name: <span>{{pkg.name}}</span></div>
+		<div>Version: <span>{{pkg.version}}</span></div>
+		<div>Description: <span>{{pkg.description}}</span></div>
+		<div>Homepage: <span><a :href="pkg.homepage" target="_blank">{{pkg.homepage}}</a></span></div>
+		<div>Repository: <span><a :href="pkg.repository.url" target="_blank">{{pkg.repository.url}}</a></span></div>
+		<div>Issues: <span><a :href="pkg.bugs.url" target="_blank">{{pkg.bugs.url}}</a></span></div>
+		<div>Author: <span>{{pkg.author}}</span></div>
+		<div>License: <span>{{pkg.license}}</span></div>
+	</div>
 </template>
+<script setup>
+import pkg from "../../package.json"
+</script>
+<style lang="scss">
+.about {
+	max-width: 600px;
+	margin: 0 auto;
+	h2 {
+		margin-top: 4rem;
+		margin-bottom: 1rem;
+	}
+	hr {
+		margin-bottom: 2rem;
+	}
+	div {
+		margin-bottom: .5rem;
+		span {
+			font-weight: 500;
+		}
+	}
+}
+</style>
